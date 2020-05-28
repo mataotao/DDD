2cd752f73f592f17fddd55536a8335b2cba31769
马涛 <matao@dustess.com>
2020-05-28 23:26:14 +0800

    eventbus

--- infrastructure/util/eventbus/eventbus.go
+++ infrastructure/util/eventbus/eventbus.go
@@ -3,16 +3,17 @@ package eventbus
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"sync"
 )
 
 //BusSubscriber 订阅
 type BusSubscriber interface {
-	Subscribe(topic string, fn interface{}) error
-	SubscribeAsync(topic string, fn interface{}, transactional bool) error
-	SubscribeOnce(topic string, fn interface{}) error
-	SubscribeOnceAsync(topic string, fn interface{}) error
-	Unsubscribe(topic string, handler interface{}) error
+	Subscribe(observer interface{}) error
+	SubscribeAsync(observer interface{}, transactional bool) error
+	SubscribeOnce(observer interface{}) error
+	SubscribeOnceAsync(observer interface{}) error
+	Unsubscribe(observer interface{}) error
 }
 
 //BusPublisher 发布
@@ -24,8 +25,8 @@ type BusPublisher interface {
 type BusController interface {
 	HasCallback(topic string) bool
 	WaitAsync()
+	Stop()
 }
-
 
 //Bus 总线
 type Bus interface {
@@ -41,6 +42,7 @@ type EventBus struct {
 }
 
 type eventHandler struct {
+	observer      interface{}
 	callBack      reflect.Value
 	flagOnce      bool
 	async         bool
@@ -58,42 +60,74 @@ func New() Bus {
 }
 
 // doSubscribe 处理订阅逻辑
-func (bus *EventBus) doSubscribe(topic string, fn interface{}, handler *eventHandler) error {
-	if !(reflect.TypeOf(fn).Kind() == reflect.Func) {
-		return fmt.Errorf("%s is not of type reflect.Func", reflect.TypeOf(fn).Kind())
-	}
+func (bus *EventBus) doSubscribe(topic string, handler *eventHandler) error {
 	handlerInterface, _ := bus.handlers.LoadOrStore(topic, make([]*eventHandler, 0))
 	handlers := handlerInterface.([]*eventHandler)
 	bus.handlers.Store(topic, append(handlers, handler))
 	return nil
 }
 
+func (bus *EventBus) checkObserver(observer interface{}) ([]string, reflect.Type, string, error) {
+	topic := make([]string, 0)
+	var t reflect.Type
+	var fn string
+	var ok bool
+
+	t = reflect.TypeOf(observer)
+	if t.Kind() != reflect.Struct {
+		return topic, t, fn, fmt.Errorf("%s is not of type reflect.Struct", t.Kind())
+	}
+	fnField, _ := t.FieldByName("Fn")
+	if fnField.Tag == "" {
+		return topic, t, fn, fmt.Errorf("%v has no field or no fn field", fnField)
+	}
+	fn, ok = fnField.Tag.Lookup("subscribe")
+	if !ok || fn == "" {
+		return topic, t, fn, fmt.Errorf("func %s doesn't exist or empty", "tag")
+	}
+	topics, ok := fnField.Tag.Lookup("topic")
+	if !ok || topics == "" {
+		return topic, t, fn, fmt.Errorf("topic %s doesn't exist or empty", "tag")
+	}
+	topic = strings.Split(topics, ",")
+	return topic, t, fn, nil
+}
+func (bus *EventBus) Register(observer interface{}, flagOnce, async, transactional bool) error {
+	topic, t, fn, err := bus.checkObserver(observer)
+	if err != nil {
+		return err
+	}
+	for i := range topic[:] {
+		function, ok := t.MethodByName(fn)
+		if !ok {
+			continue
+		}
+		_ = bus.doSubscribe(topic[i], &eventHandler{
+			observer, function.Func, flagOnce, async, transactional, new(sync.Mutex),
+		})
+	}
+	return nil
+}
+
 // Subscribe 订阅-同步
-func (bus *EventBus) Subscribe(topic string, fn interface{}) error {
-	return bus.doSubscribe(topic, fn, &eventHandler{
-		reflect.ValueOf(fn), false, false, false, new(sync.Mutex),
-	})
+func (bus *EventBus) Subscribe(observer interface{}) error {
+	return bus.Register(observer, false, false, false)
 }
 
 // SubscribeAsync  订阅-异步
-func (bus *EventBus) SubscribeAsync(topic string, fn interface{}, transactional bool) error {
-	return bus.doSubscribe(topic, fn, &eventHandler{
-		reflect.ValueOf(fn), false, true, transactional, new(sync.Mutex),
-	})
+func (bus *EventBus) SubscribeAsync(observer interface{}, transactional bool) error {
+	return bus.Register(observer, false, true, transactional)
+
 }
 
 // SubscribeOnce 订阅-只执行一次-同步
-func (bus *EventBus) SubscribeOnce(topic string, fn interface{}) error {
-	return bus.doSubscribe(topic, fn, &eventHandler{
-		reflect.ValueOf(fn), true, false, false, new(sync.Mutex),
-	})
+func (bus *EventBus) SubscribeOnce(observer interface{}) error {
+	return bus.Register(observer, true, false, false)
 }
 
 // SubscribeOnceAsync 订阅-只执行一次-异步
-func (bus *EventBus) SubscribeOnceAsync(topic string, fn interface{}) error {
-	return bus.doSubscribe(topic, fn, &eventHandler{
-		reflect.ValueOf(fn), true, true, false, new(sync.Mutex),
-	})
+func (bus *EventBus) SubscribeOnceAsync(observer interface{}) error {
+	return bus.Register(observer, true, true, false)
 }
 
 // HasCallback 查看事件订阅的函数
@@ -107,55 +141,19 @@ func (bus *EventBus) HasCallback(topic string) bool {
 }
 
 // Unsubscribe 删除订阅
-func (bus *EventBus) Unsubscribe(topic string, handler interface{}) error {
-
-	if handlersInterface, ok := bus.handlers.Load(topic); ok {
-		handlers := handlersInterface.([]*eventHandler)
-		if len(handlers) > 0 {
-			bus.removeHandler(topic, bus.findHandlerIdx(topic, reflect.ValueOf(handler)))
-		}
-		return nil
+func (bus *EventBus) Unsubscribe(observer interface{}) error {
+	topic, t, fn, err := bus.checkObserver(observer)
+	if err != nil {
+		return err
 	}
-	return fmt.Errorf("topic %s doesn't exist", topic)
-}
-
-// Publish 推送
-func (bus *EventBus) Publish(topic string, args ...interface{}) {
-
-	if handlerInterface, ok := bus.handlers.Load(topic); ok {
-		handlers := handlerInterface.([]*eventHandler)
-		if len(handlers) == 0 {
-			return
+	for i := range topic[:] {
+		function, ok := t.MethodByName(fn)
+		if !ok {
+			continue
 		}
-
-		for i := range handlers[:] {
-			if handlers[i].flagOnce {
-				bus.removeHandler(topic, i)
-			}
-			if !handlers[i].async {
-				bus.doPublish(handlers[i], topic, args...)
-			} else {
-				bus.wg.Add(1)
-				if handlers[i].transactional {
-					handlers[i].Lock()
-				}
-				go bus.doPublishAsync(handlers[i], topic, args...)
-			}
-		}
+		bus.removeHandler(topic[i], bus.findHandlerIdx(topic[i], function.Func))
 	}
-}
-
-func (bus *EventBus) doPublish(handler *eventHandler, topic string, args ...interface{}) {
-	passedArguments := bus.setUpPublish(handler, args...)
-	handler.callBack.Call(passedArguments)
-}
-
-func (bus *EventBus) doPublishAsync(handler *eventHandler, topic string, args ...interface{}) {
-	defer bus.wg.Done()
-	if handler.transactional {
-		defer handler.Unlock()
-	}
-	bus.doPublish(handler, topic, args...)
+	return nil
 }
 
 func (bus *EventBus) removeHandler(topic string, idx int) {
@@ -175,11 +173,46 @@ func (bus *EventBus) removeHandler(topic string, idx int) {
 	} else {
 		bus.handlers.Delete(topic)
 	}
+}
 
+// Publish 推送
+func (bus *EventBus) Publish(topic string, args ...interface{}) {
+	if handlerInterface, ok := bus.handlers.Load(topic); ok {
+		handlers := handlerInterface.([]*eventHandler)
+		if len(handlers) == 0 {
+			return
+		}
+		for i, handler := range handlers {
+			if handler.flagOnce {
+				bus.removeHandler(topic, i)
+			}
+			if !handler.async {
+				bus.doPublish(handler, args...)
+			} else {
+				bus.wg.Add(1)
+				if handler.transactional {
+					handler.Lock()
+				}
+				go bus.doPublishAsync(handlers[i], args...)
+			}
+		}
+	}
+}
+
+func (bus *EventBus) doPublish(handler *eventHandler, args ...interface{}) {
+	passedArguments := bus.setUpPublish(handler, args...)
+	handler.callBack.Call(passedArguments)
+}
+
+func (bus *EventBus) doPublishAsync(handler *eventHandler, args ...interface{}) {
+	defer bus.wg.Done()
+	if handler.transactional {
+		defer handler.Unlock()
+	}
+	bus.doPublish(handler, args...)
 }
 
 func (bus *EventBus) findHandlerIdx(topic string, callback reflect.Value) int {
-
 	if handlerInterface, ok := bus.handlers.Load(topic); ok {
 		handlers := handlerInterface.([]*eventHandler)
 		for i := range handlers[:] {
@@ -194,12 +227,13 @@ func (bus *EventBus) findHandlerIdx(topic string, callback reflect.Value) int {
 
 func (bus *EventBus) setUpPublish(callback *eventHandler, args ...interface{}) []reflect.Value {
 	funcType := callback.callBack.Type()
-	passedArguments := make([]reflect.Value, len(args))
+	passedArguments := make([]reflect.Value, 0, len(args)+1)
+	passedArguments = append(passedArguments, reflect.ValueOf(callback.observer))
 	for i := range args[:] {
 		if args[i] == nil {
-			passedArguments[i] = reflect.New(funcType.In(i)).Elem()
+			passedArguments = append(passedArguments, reflect.New(funcType.In(i)).Elem())
 		} else {
-			passedArguments[i] = reflect.ValueOf(args[i])
+			passedArguments = append(passedArguments, reflect.ValueOf(args[i]))
 		}
 	}
 
@@ -209,4 +243,43 @@ func (bus *EventBus) setUpPublish(callback *eventHandler, args ...interface{}) [
 // WaitAsync 等待
 func (bus *EventBus) WaitAsync() {
 	bus.wg.Wait()
+}
+
+// WaitAsync 等待
+func (bus *EventBus) Stop() {
+	bus.handlers.Range(func(topic, value interface{}) bool {
+		handlers := value.([]*eventHandler)
+		for i := range handlers[:] {
+			t, fn, ok := bus.checkStop(handlers[i].observer)
+			if !ok {
+				return false
+			}
+			function, ok := t.MethodByName(fn)
+			if !ok {
+				continue
+			}
+			function.Func.Call([]reflect.Value{reflect.ValueOf(handlers[i].observer)})
+			_ = bus.Unsubscribe(handlers[i].observer)
+		}
+
+		return true
+	})
+}
+func (bus *EventBus) checkStop(observer interface{}) (reflect.Type, string, bool) {
+	var fn string
+	var t reflect.Type
+	t = reflect.TypeOf(observer)
+	if t.Kind() != reflect.Struct {
+		return t, fn, false
+	}
+	fnField, _ := t.FieldByName("Fn")
+	if fnField.Tag == "" {
+		return t, fn, false
+	}
+	fn, ok := fnField.Tag.Lookup("stop")
+	if !ok || fn == "" {
+		return t, fn, false
+	}
+
+	return t, fn, true
 }
